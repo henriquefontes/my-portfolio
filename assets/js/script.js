16643ded32bcd61fffedfe5b98f41d9873089cb7
Henrique Luiz <hnrq.luiz1@gmail.com>
2021-09-26 01:24:10 -0300

    add: banner type effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,58 @@ const handlePageScroll = () => {
     : $header.classList.remove('header--background')
 }
 
+const typeMachine = ({ element, messages, interval, loop }) => {
+  let typeEffect
+
+  const renderMessageIntoDOM = () => {
+    let currentMessageIndex = 0
+    let currentLetterIndex = 0
+    let currentMessage = ''
+    let currentLetters = ''
+
+    const renderWord = () => {
+      const isLastMessage = currentMessageIndex == messages.length
+
+      if (isLastMessage) {
+        if (loop) {
+          currentMessageIndex = 0
+        } else {
+          clearInterval(typeEffect)
+          return
+        }
+      }
+
+      currentMessage = messages[currentMessageIndex]
+      currentLetters = currentMessage.slice(0, currentLetterIndex++)
+
+      const isLastLetter = currentLetters.length == currentMessage.length
+
+      element.textContent = currentLetters
+
+      if (isLastLetter) {
+        currentMessageIndex++
+        currentLetterIndex = 0
+      }
+
+    }
+
+    typeEffect = setInterval(renderWord, interval)
+  }
+
+  renderMessageIntoDOM()
+}
+
+const bannerMessages = [
+  "Web developer",
+  "Games enjoyer",
+  "Zula Hater"
+]
+
+const typeMessages = typeMachine({
+  element: document.getElementById('work'),
+  messages: bannerMessages,
+  interval: 200,
+  loop: true
+})
+
 window.addEventListener('scroll', handlePageScroll)
